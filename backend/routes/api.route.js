d84f5c539135ab47234762e3a4900d8ba8a4bf7a
easymagic <nnamware@yahoo.com>
2022-04-08 00:42:55 +0100

    np

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,7 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+router.get("/", async (req, res, next) => {
+  res.send({ message: "Ok api is working 🚀" });
+});
+
+//Category
+router.get("/contacts", async (req, res, next) => {
+  res.send({});
+});
+router.get("/contacts/:id", async (req, res, next) => {
+  res.send({});
+});
+router.put("/contacts/:id", async (req, res, next) => {
+  res.send({});
+});
+router.delete("/contacts/:id", async (req, res, next) => {
+  res.send({});
+});
+router.post("/contacts", async (req, res, next) => {
+  res.send({});
 });
 
 module.exports = router;
